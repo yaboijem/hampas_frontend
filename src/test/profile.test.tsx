9f2e7f3e5c4006879d23b93a95b402de7e00d2b1
yaboijem <bantoljeremy27@gmail.com>
2026-08-17 17:14:55 +0800

    test: loosen profile greeting matcher for hello punctuation

--- src/test/profile.test.tsx
+++ src/test/profile.test.tsx
@@ -66,7 +66,7 @@ describe('ProfilePage', () => {
       </MemoryRouter>,
     );
 
-    expect(await screen.findByRole('heading', { name: /hello, jem player/i })).toBeInTheDocument();
+    expect(await screen.findByRole('heading', { name: /hello[!,]?\s*jem player/i })).toBeInTheDocument();
     expect(screen.queryByLabelText(/^name$/i)).not.toBeInTheDocument();
     expect(screen.queryByLabelText(/^setter$/i)).not.toBeInTheDocument();
 
